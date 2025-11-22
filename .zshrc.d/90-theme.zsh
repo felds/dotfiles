694f777e4862cf374fba0f66c5a21c1bef084029
Felds Liscia <dev@felds.com.br>
2025-11-21 22:14:44 -0300

    path from git root

--- .zshrc.d/90-theme.zsh
+++ .zshrc.d/90-theme.zsh
@@ -29,6 +29,17 @@ git_prompt_info() {
 	echo " git:${branch}${dirty_mark}"
 }
 
+prompt_path() {
+	local repo_root
+	if repo_root=$(git rev-parse --show-toplevel 2> /dev/null); then
+		local rel="${PWD#$repo_root}"
+		rel=${rel#/}
+		printf '%s' "${repo_root##*/}"
+		[[ -n $rel ]] && printf '/%s' "$rel"
+	else
+		printf '%s' "%~"
+	fi
+}
 
 PROMPT=""
 PROMPT+=$'\n'
@@ -36,8 +47,9 @@ PROMPT+='%F{242}%n%f'  # username
 PROMPT+='@'
 PROMPT+='%m'  # hostname
 PROMPT+=' '
-PROMPT+='%F{magenta}%~%f'  # path from ~
-PROMPT+='$(git_prompt_info)'  # Status do Git
+# PROMPT+='%F{magenta}%3~%f'  # path from ~; max 3 parts
+PROMPT+='%F{magenta}$(prompt_path)%f'
+PROMPT+='$(git_prompt_info)'
 PROMPT+='%(?..%F{red} [%?]%f)'  # error code, if any
 PROMPT+=$'\n'
 PROMPT+='❯ '
